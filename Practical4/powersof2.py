a2ce765ea502cf68ebb728b9f1a6181254a7a571
Qile Guo <Qile.18@intl.zju.edu.cn>
2019-03-20 10:41:36 +0800

    try to improve

--- Practical4/powersof2.py
+++ Practical4/powersof2.py
@@ -10,6 +10,7 @@ Created on Wed Mar 13 10:11:01 2019
 #repeat the last two steps and find n2
 #repeat to find n1,n2,n3…until nx=1
 #output the result"x= 2**n1 +2**n2 + … +2**1
+
 x=2019
 for i in range(1,14):
     if x <= 2**i and x >= 2**(i-1):
@@ -18,6 +19,18 @@ for i in range(1,14):
 while x != 0:
     for i in range(1,14):
         if x <= 2**i and x >= 2**(i-1):
+            x= x-2**(i-1)
+            a=a + '+' '2**'+ str(i-1)
+print("x"+ "=" + a)
+
+x=2015
+for i in range(14,1,-1):
+    if  x >= 2**(i-1):
+        a='2**'+ str(i-1)
+        x= x-2**(i-1)
+while x != 0:
+    for i in range(14,1,-1):
+        if x >= 2**(i-1):
             x= x-2**(i-1)
             a=a + '+' '2**'+ str(i-1)
 print("x"+ "=" + a)
